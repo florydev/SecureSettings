3f700a804f901f423a2bf086b0df7b54528ce3cd
Mark Flory <mark@florydev.com>
2024-10-26 16:24:11 -0500

    Initial changes

---
+++ Interfaces/IDecrypter.cs
@@ -0,0 +1,8 @@
+﻿namespace FloryDev.SecureSettings.Interfaces
+{
+    public interface IDecrypter
+    {
+        string DecryptString(string encryptedString);
+        bool ValueIsEncrypted(string value);
+    }
+}
